cbf063abe308fac68d240cc61475c3a462310924
artem.ivanov <artem.ivanov@dsr-company.com>
2020-10-14 16:54:22 +0300

    Scripts for VCX testing
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- vcx/wrappers/python3/demo/faber.py
+++ vcx/wrappers/python3/demo/faber.py
@@ -7,6 +7,8 @@ from ctypes import cdll
 from time import sleep
 
 from demo_utils import file_ext
+
+from demo.demo_utils import download_message
 from vcx.api.connection import Connection
 from vcx.api.credential_def import CredentialDef
 from vcx.api.issuer_credential import IssuerCredential
@@ -15,8 +17,6 @@ from vcx.api.schema import Schema
 from vcx.api.utils import vcx_agent_provision, vcx_get_ledger_author_agreement, vcx_set_active_txn_author_agreement_meta
 from vcx.api.vcx_init import vcx_init_with_config
 from vcx.state import State, ProofState
-
-TAA_ACCEPT = bool(os.getenv("TAA_ACCEPT", "0") == "1")
 
 # logging.basicConfig(level=logging.DEBUG) uncomment to get logs
 
@@ -27,9 +27,9 @@ TAA_ACCEPT = bool(os.getenv("TAA_ACCEPT", "0") == "1")
 # 'wallet_key': encryption key for encoding wallet
 # 'payment_method': method that will be used for payments
 provisionConfig = {
-    'agency_url': 'http://localhost:8080',
-    'agency_did': 'VsKV7grR1BUE29mG2Fm2kX',
-    'agency_verkey': 'Hezce2UWMZ3wUhVkh2LfKSs8nDzWwzs2Win7EzNN3YaR',
+    'agency_url': 'https://eas-team1.pdev.evernym.com',
+    'agency_did': 'CV65RFpeCtPu82hNF9i61G',
+    'agency_verkey': '7G3LhXFKXKTMv7XGx1Qc9wqkMbwcU2iLBHL8x1JXWWC2',
     'wallet_name': 'faber_wallet',
     'wallet_key': '123',
     'payment_method': 'null',
@@ -47,14 +47,30 @@ async def main():
     config = json.loads(config)
     # Set some additional configuration options specific to faber
     config['institution_name'] = 'Faber'
-    config['institution_logo_url'] = 'http://robohash.org/234'
+    config['institution_logo_url'] = 'http://robohash.org/1'
     config['genesis_path'] = 'docker.txn'
     config['payment_method'] = 'null'
 
     print("#2 Initialize libvcx with new configuration")
     await vcx_init_with_config(json.dumps(config))
 
-    connection_to_alice = None
+    print("#5 Create a connection to alice and print out the invite details")
+    connection_to_alice = await Connection.create('alice')
+    await connection_to_alice.connect('{"use_public_did": true}')
+    await connection_to_alice.update_state()
+    details = await connection_to_alice.invite_details(False)
+    print("**invite details**")
+    print(json.dumps(details))
+    print("******************")
+
+    print("#6 Poll agency and wait for alice to accept the invitation (start alice.py now)")
+    connection_state = await connection_to_alice.get_state()
+    while connection_state != State.Accepted:
+        sleep(2)
+        await connection_to_alice.update_state()
+        connection_state = await connection_to_alice.get_state()
+
+    print("Connection is established")
 
     while True:
         answer = input(
@@ -67,9 +83,7 @@ async def main():
             "5 - establish out-of-band connection \n "
             "else finish \n") \
             .lower().strip()
-        if answer == '0':
-            connection_to_alice = await connect()
-        elif answer == '1':
+        if answer == '1':
             await issue_credential(connection_to_alice)
         elif answer == '2':
             await ask_for_proof(connection_to_alice, config['institution_did'])
@@ -82,6 +96,8 @@ async def main():
                 connection_state = await connection_to_alice.get_state()
                 print("State: " + str(connection_state))
         elif answer == '4':
+            pw_did = await connection_to_alice.get_my_pw_did()
+            uid, offer, _ = await download_message(pw_did, 'handshake-reuse')
             await connection_to_alice.update_state()
         elif answer == '5':
             connection_to_alice = await outofband_connect()
@@ -131,22 +147,26 @@ async def outofband_connect():
 
     print("Connection is established")
     return connection_to_alice
-    
+
+
+async def accept_taa():
+    # To support ledger which transaction author agreement accept needed
+    print("#2.1 Accept transaction author agreement")
+    txn_author_agreement = await vcx_get_ledger_author_agreement()
+    txn_author_agreement_json = json.loads(txn_author_agreement)
+    first_acc_mech_type = list(txn_author_agreement_json['aml'].keys())[0]
+    vcx_set_active_txn_author_agreement_meta(text=txn_author_agreement_json['text'],
+                                             version=txn_author_agreement_json['version'],
+                                             hash=None,
+                                             acc_mech_type=first_acc_mech_type, time_of_acceptance=int(time.time()))
+
 
 async def issue_credential(connection_to_alice):
-    if TAA_ACCEPT:
-        # To support ledger which transaction author agreement accept needed
-        print("#2.1 Accept transaction author agreement")
-        txn_author_agreement = await vcx_get_ledger_author_agreement()
-        txn_author_agreement_json = json.loads(txn_author_agreement)
-        first_acc_mech_type = list(txn_author_agreement_json['aml'].keys())[0]
-        vcx_set_active_txn_author_agreement_meta(text=txn_author_agreement_json['text'], version=txn_author_agreement_json['version'],
-                                                 hash=None,
-                                                 acc_mech_type=first_acc_mech_type, time_of_acceptance=int(time.time()))
+    await accept_taa()
 
     print("#3 Create a new schema on the ledger")
     version = format("%d.%d.%d" % (random.randint(1, 101), random.randint(1, 101), random.randint(1, 101)))
-    schema = await Schema.create('schema_uuid', 'degree schema', version, ['email', 'first_name', 'last_name'], 0)
+    schema = await Schema.create('schema_uuid', 'degree schema', version, ['Email', 'First Name', 'Last Name', 'Age', 'Sex', 'DateofBirth'], 0)
     schema_id = await schema.get_schema_id()
 
     print("#4 Create a new credential definition on the ledger")
@@ -154,13 +174,16 @@ async def issue_credential(connection_to_alice):
     cred_def_handle = cred_def.handle
 
     schema_attrs = {
-        'email': 'test',
-        'first_name': 'DemoName',
-        'last_name': 'DemoLastName',
+        'Email': '003',
+        'First Name': 'Faber',
+        'Last Name': 'Test',
+        'Age': '22',
+        'Sex': 'male',
+        'DateofBirth':'2000-02-04',
     }
 
     print("#12 Create an IssuerCredential object using the schema and credential definition")
-    credential = await IssuerCredential.create('alice_degree', schema_attrs, cred_def_handle, 'cred', '0')
+    credential = await IssuerCredential.create('alice_degree', schema_attrs, cred_def_handle, 'Demo Credential', '0')
 
     print("#13 Issue credential offer to alice")
     await credential.send_offer(connection_to_alice)
@@ -196,13 +219,12 @@ async def issue_credential(connection_to_alice):
 
 async def ask_for_proof(connection_to_alice, institution_did):
     proof_attrs = [
-        {'name': 'email', 'restrictions': [{'issuer_did': institution_did}]},
-        {'name': 'first_name', 'restrictions': [{'issuer_did': institution_did}]},
-        {'name': 'last_name', 'restrictions': [{'issuer_did': institution_did}]}
+        {'name': 'Email', 'restrictions': {'issuer_did': institution_did}},
+        {'names': ['First Name', 'Last Name'], 'restrictions': {'issuer_did': institution_did}},
     ]
 
     print("#19 Create a Proof object")
-    proof = await Proof.create('proof_uuid', 'proof_from_alice', proof_attrs, {})
+    proof = await Proof.create('proof_uuid', 'Person Proving', proof_attrs, {})
 
     print("#20 Request proof of degree from alice")
     await proof.request_proof(connection_to_alice)
@@ -227,6 +249,8 @@ async def ask_for_proof(connection_to_alice, institution_did):
         print("proof is verified!!")
     else:
         print("could not verify proof :(")
+
+    print(await proof.serialize())
 
 
 if __name__ == '__main__':
